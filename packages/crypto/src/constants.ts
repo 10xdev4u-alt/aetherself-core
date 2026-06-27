7cbc61c91b40a63809c80929feb0201104f41fb6
10xdev4u-alt <10xdev4u@gmail.com>
2026-06-28 01:51:34 +0530

    feat(crypto): add crypto constants

---
+++ packages/crypto/src/constants.ts
@@ -0,0 +1,6 @@
+export const ED25519_KEY_SIZE = 32;
+export const ED25519_SIG_SIZE = 64;
+export const XCHACHA20_KEY_SIZE = 32;
+export const XCHACHA20_NONCE_SIZE = 24;
+export const SALT_SIZE = 32;
+export const PBKDF2_ITERATIONS = 600_000;
